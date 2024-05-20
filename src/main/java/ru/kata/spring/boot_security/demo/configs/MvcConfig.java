10384adecf3849846178a5366b713eabfaf28ee2
Sarah Mark <sarahhhmark@gmail.com>
2024-05-20 14:02:50 +0700

    (×﹏×)

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -1,12 +1,23 @@
 package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user").setViewName("user");
     }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/templates/js/")
+                .setCachePeriod(3)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
 }
+
--- src/main/resources/js/edit-validation.js
+++
@@ -1,5 +0,0 @@
-const editBtn = document.getElementById('editButton')
-editBtn.addEventListener('click', function (event)  {
-    event.stopPropagation()
-    console.log('********************* Hello from JS! *********************', event.type)
-})
